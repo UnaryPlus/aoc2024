47187cc6d9005a99d19a91c3079677f6c29392ee
UnaryPlus <ombspring@gmail.com>
2024-12-06 11:20:27 -0500

    use array instead of vector

--- src/AoC2024/Solutions/Day4.hs
+++ src/AoC2024/Solutions/Day4.hs
@@ -1,22 +1,24 @@
 module AoC2024.Solutions.Day4 (parse, part1, part2) where 
 
-import Data.Vector (Vector, (!))
-import qualified Data.Vector as Vec
+import Data.Array (Array, (!))
+import qualified Data.Array as Array
 import AoC2024.Utils (count, (|||))
 
-type Grid a = Vector (Vector a)
+type Grid a = Array (Int, Int) a
+
+fromList :: [[a]] -> Grid a
+fromList rows =
+  let (m, n) = (length rows, length (head rows)) in
+  Array.listArray ((0, 0), (m - 1, n - 1)) (concat rows)
 
 dims :: Grid a -> (Int, Int)
-dims grid = (Vec.length grid, Vec.length (grid ! 0))
-
-entry :: (Int, Int) -> Grid a -> a
-entry (i, j) grid = grid ! i ! j
+dims = add (1, 1) . snd . Array.bounds
 
 add :: (Int, Int) -> (Int, Int) -> (Int, Int)
 add (i1, j1) (i2, j2) = (i1 + i2, j1 + j2)
 
 applyMask :: Functor f => (Int, Int) -> f (Int, Int) -> Grid a -> f a
-applyMask base mask grid = fmap (\ix -> entry (add base ix) grid) mask
+applyMask base mask grid = fmap (\ix -> grid ! add base ix) mask
 
 slideMask :: Functor f => (Int, Int) -> (Int, Int) -> f (Int, Int) -> Grid a -> [f a]
 slideMask (i0, j0) (i1, j1) mask grid = [ applyMask (i, j) mask grid | i <- [i0..i1], j <- [j0..j1] ]
@@ -25,7 +27,7 @@ slideMasks :: Functor f => [((Int, Int), (Int, Int), f (Int, Int))] -> Grid a ->
 slideMasks masks grid = concatMap (\(ix0, ix1, mask) -> slideMask ix0 ix1 mask grid) masks
 
 parse :: String -> Grid Char
-parse = Vec.fromList . map Vec.fromList . lines
+parse = fromList . lines
 
 part1 :: Grid Char -> Int
 part1 grid = let
@@ -41,7 +43,7 @@ part1 grid = let
 part2 :: Grid Char -> Int
 part2 grid = let
   (m, n) = dims grid
-  mask = Vec.fromList [(-1, -1), (-1, 1), (0, 0), (1, -1), (1, 1)]
+  mask = Array.listArray (0 :: Int, 4) [(-1, -1), (-1, 1), (0, 0), (1, -1), (1, 1)]
   exes = slideMask (1, 1) (m - 2, n - 2) mask grid
   ms = [('M', 'S'), ('S', 'M')]
   isXmas ex = ex ! 2 == 'A' && (ex ! 0, ex ! 4) `elem` ms && (ex ! 1, ex ! 3) `elem` ms
